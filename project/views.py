08c5baaf789511cf01ff9e64c3b5cb4029df05dd
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-06-17 01:08:22 -0700

    Fix missing APIView import

--- project/views.py
+++ project/views.py
@@ -37,6 +37,7 @@ from rest_framework import generics, viewsets, status
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import (
     Project, ProjectTemplate, ScopeOfWork, ProjectMaterial,
