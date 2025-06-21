bba1deb57c4ad8bc4f1bb06e8e61aa27fe54421b
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-06-21 01:30:55 -0700

    Move inline styles to static files

---
+++ static/home/js/messages.js
@@ -0,0 +1,6 @@
+/* ============================================
+   Generic message helpers
+   ============================================ */
+$(document).ready(function() {
+    setTimeout(function() { $('.alert').fadeOut(); }, 5000);
+});
