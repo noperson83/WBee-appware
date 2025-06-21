b9e7c0b268c05e14829c7868d359ccda633d959e
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-06-21 00:57:44 -0700

    Add schedule index view

--- schedule/urls.py
+++ schedule/urls.py
@@ -6,6 +6,8 @@ from . import views
 app_name = "schedule"
 
 urlpatterns = [
+    path("", views.CalendarListView.as_view(), name="schedule"),
+    path("calendars/", views.CalendarListView.as_view(), name="calendar_list"),
     path("<int:calendar_id>/ical/", CalendarICalendar(), name="calendar-ical"),
     path("event/create/<str:proj>/", views.EventCreateView.as_view(), name="create-event"),
     path("calendars/<slug:slug>/", views.CalendarDetailView.as_view(), name="calendar-detail"),
