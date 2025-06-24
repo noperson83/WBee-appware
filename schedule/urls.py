9ff91a34bfbceb851f2057e8b2826482b84ead38
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-06-23 23:02:31 -0700

    Add calendar-specific event creation route

--- schedule/urls.py
+++ schedule/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
     path("<int:calendar_id>/ical/", CalendarICalendar(), name="calendar-ical"),
     path("events/<int:pk>/", views.EventDetailView.as_view(), name="event-detail"),
     path("event/create/<str:proj>/", views.EventCreateView.as_view(), name="create-event"),
+    path(
+        "calendars/<slug:slug>/create-event/",
+        views.EventCreateView.as_view(),
+        name="calendar_create_event",
+    ),
     path("calendars/<slug:slug>/day/", views.DayCalendarView.as_view(), name="day_calendar"),
     path("calendars/<slug:slug>/week/", views.WeekCalendarView.as_view(), name="week_calendar"),
     path("calendars/<slug:slug>/year/", views.YearCalendarView.as_view(), name="year_calendar"),
