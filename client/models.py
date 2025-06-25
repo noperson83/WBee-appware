777beeb0fc5887e7ce2ac57f39305445ac3ca49a
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-06-24 19:54:32 -0700

    Allow formatted phone numbers

--- client/models.py
+++ client/models.py
@@ -82,8 +82,8 @@ class Contact(TimeStampedModel):
     
     # Phone with validation
     phone_regex = RegexValidator(
-        regex=r'^\+?1?\d{9,15}$',
-        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+        regex=r'^\+?[\d\s().-]{7,20}$',
+        message="Enter a valid phone number."
     )
     phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     email = models.EmailField(blank=True)
