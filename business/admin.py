c0f85d70c8cd14df499f84139a37d4617159eb11
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-06-25 19:23:41 -0700

    Add admin interface for BusinessConfiguration

---
+++ business/admin.py
@@ -0,0 +1,82 @@
+from django.contrib import admin
+
+from .models import BusinessConfiguration
+
+
+@admin.register(BusinessConfiguration)
+class BusinessConfigurationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'deployment_type',
+        'billing_model',
+        'enables_shared_inventory',
+        'enables_shared_workforce',
+        'enables_shared_clients',
+        'enables_cross_selling',
+        'created_at',
+    )
+
+    list_filter = (
+        'deployment_type',
+        'billing_model',
+        'enables_shared_inventory',
+        'enables_shared_workforce',
+        'enables_shared_clients',
+        'enables_cross_selling',
+        'created_at',
+    )
+
+    search_fields = (
+        'name',
+        'description',
+        'industry_details',
+    )
+
+    readonly_fields = (
+        'slug',
+        'created_at',
+        'updated_at',
+    )
+
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
+
+    fieldsets = (
+        ('General', {
+            'fields': (
+                'name',
+                'slug',
+                'description',
+                'industry_details',
+            )
+        }),
+        ('Business Model', {
+            'fields': (
+                'deployment_type',
+                'billing_model',
+            )
+        }),
+        ('Sharing Capabilities', {
+            'fields': (
+                'enables_shared_inventory',
+                'enables_shared_workforce',
+                'enables_shared_clients',
+                'enables_cross_selling',
+            ),
+            'classes': ('collapse',),
+        }),
+        ('Workflow', {
+            'fields': (
+                'workflow_requirements',
+            ),
+            'classes': ('collapse',),
+        }),
+        ('System Information', {
+            'fields': (
+                'created_at',
+                'updated_at',
+            ),
+            'classes': ('collapse',),
+        }),
+    )
